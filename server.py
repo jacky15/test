8e4dba96420c8398d422450d5106b8c642422972
jacky15 <jacky15.leung@gmail.com>
2015-05-25 22:49:22 +0800

    add the url dispatch

--- server.py
+++ server.py
@@ -23,7 +23,7 @@ class TcpServer(object):
     #define the family of the server
     __address_famliy = socket.AF_INET
 
-    def __init__(self,address,port,handler_class,address_famliy=None,reuse_address=True,max_queue_size=1,poll_gap=0.5):
+    def __init__(self,address,port,handler_class,address_famliy=None,reuse_address=True,max_queue_size=5,poll_gap=0.5):
         '''init the class'''
         if address_famliy:
             self.__address_famliy = address_famliy
@@ -183,15 +183,61 @@ class BaseHttpHandler(MyTcpHandler):
     #可以被映射的方法
     ALLOW_METHOD = ['GET','POST']
     #解析用户请求
-    PATH_RE = re.compile('(?P<path>(/.*?))(\?(?P<params>(.*)))?')
-    #PARAM_RE = re.compile('((.*?)=(.*?))&?*')
-    command = ''
+    PATH_RE = re.compile('/(?P<path>(.*))(\?(?P<params>(.*)))?')
+    #URL dispatch map 
+    #should be overwirte
+    URL_DISPATCH = ()
+
+    request_command = ''
     request_path = ''
     request_param = {
         'GET' : {},
         'POST' : {}
     }
     head_info = {}
+
+    ERROR_CODE_MAP = {
+          "100": "Continue",
+          "101": "Switching Protocols",
+          "200": "OK",
+          "201": "Created",
+          "202": "Accepted",
+          "203": "Non-Authoritative Information",
+          "204": "No Content",
+          "205": "Reset Content",
+          "206": "Partial Content",
+          "300": "Multiple Choices",
+          "301": "Moved Permanently",
+          "302": "Found",
+          "303": "See Other",
+          "304": "Not Modified",
+          "305": "Use Proxy",
+          "307": "Temporary Redirect",
+          "400": "Bad Request",
+          "401": "Unauthorized",
+          "402": "Payment Required",
+          "403": "Forbidden",
+          "404": "Not Found",
+          "405": "Method Not Allowed",
+          "406": "Not Acceptable",
+          "407": "Proxy Authentication Required",
+          "408": "Request Time-out",
+          "409": "Conflict",
+          "410": "Gone",
+          "411": "Length Required",
+          "412": "Precondition Failed",
+          "413": "Request Entity Too Large",
+          "414": "Request-URI Too Large",
+          "415": "Unsupported Media Type",
+          "416": "Requested range not satisfiable",
+          "417": "Expectation Failed",
+          "500": "Internal Server Error",
+          "501": "Not Implemented",
+          "502": "Bad Gateway",
+          "503": "Service Unavailable",
+          "504": "Gateway Time-out",
+          "505": "HTTP Version not supported"
+    }
 
     def handle(self):
         '''此函数用于解析http请求首行，用于分发各个方法进行处理，不关心详细处理细节'''
@@ -200,13 +246,18 @@ class BaseHttpHandler(MyTcpHandler):
 
         raw_request_line = self.rfile.readline(65535)
 
+        print raw_request_line
         if not self.analysis_request(raw_request_line):
             return self.request_error('request error')
 
         
         #判断这个请求是何种请求
-        method = getattr(self,self.command.lower())
-        method()
+        #method = getattr(self,self.command.lower())
+        #method()
+        #update by jackyliang@20150524,added the function to dispatch the url path
+        method = self.request_dispatch()
+        response_content = method()
+        self.send_response(response_content)
         self.wfile.flush()
 
     def analysis_request(self,raw_request_line):
@@ -217,7 +268,7 @@ class BaseHttpHandler(MyTcpHandler):
         if not command.upper() in self.ALLOW_METHOD:
             return False
         
-        self.command = command
+        self.request_command = command
         self.request_path = request_path
 
         #过滤请求路径异常的请求
@@ -268,35 +319,79 @@ class BaseHttpHandler(MyTcpHandler):
             print _request_param,int(_content_length)
             self.analysis_param('POST',_request_param)
 
-    def request_error(self,message):
+    def request_error(self,error_code,message):
         '''返回错误信息'''
-        return self.wfile.write(json.dumps({'result' : False}))
+        _temp_response_str = 'HTTP/1.1 %s %s\r\n\r\n' % (error_code,self.ERROR_CODE_MAP.get(error_code,'UNKOWN'))
+        _temp_response_str += json.dumps({'result' : False}) 
+        self.wfile.write(_temp_response_str)
 
-class HttpHandler(BaseHttpHandler):
-    
+    def request_405_error(self):
+        '''return the message that the method not allow'''
+        self.request_error('405','method not allowed')        
+
+    def request_404_error(self):
+        '''return the message that request path not found'''
+        self.request_error('404','not found') 
+
+    def send_response(self,response_content):
+        '''send the response to the client'''
+        self.wfile.write(response_content)
+
+    def request_dispatch(self):
+        '''dispatch the url to target function'''
+        #find the target class
+        method = self.request_404_error
+        for url_map in self.URL_DISPATCH:
+            _temp_url_re = url_map[0]
+            _temp_url_class = url_map[1]
+
+            if re.match(_temp_url_re,self.request_path):
+                #find the target method
+                _temp_url_class_inst = _temp_url_class(self.head_info,self.request_param)     
+                method = getattr(_temp_url_class_inst,self.request_command.lower(),self.request_405_error)
+                #once found the class match the request path ,
+                #no longer search is need
+                break
+                
+        return method
+                  
+class BaseView():
+
+    def __init__(self,head_info,request_param):
+        self.head_info = head_info
+        self.request_param = request_param
+        
+
+class Iter2Handler(BaseView):
+
     def get(self):
-        print 'In GET function.'
-        print 'request path : %s' % (self.request_path)
-        print self.head_info
-        self.wfile.write('hello world\n')
-        result = json.dumps(self.request_param['GET'])
-        self.wfile.write(result + '\n')
-
+        #print 'In GET function.'
+        #print 'request path : %s' % ('/jacky/')
+        #print self.head_info
+        response_content = 'hello world\n'
+        response_content += json.dumps(self.request_param['GET'])
+        return response_content
 
     def post(self):
-        print 'In POST function.'
-        print 'request path : %s' % (self.request_path)
-        self.wfile.write('hello world\n')
+        #print 'In POST function.'
+        #print 'request path : %s' % ('/jacky/')
+        response_content = 'hello world\n'
         self.request_param['POST']['time'] = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        result = json.dumps(self.request_param['POST'])
-        self.wfile.write(result + '\n')
+        response_content += json.dumps(self.request_param['POST'])
+        return response_content
 
+class MyHttpHandler(BaseHttpHandler):
+    URL_DISPATCH = (
+        ('jacky',Iter2Handler),
+    )
 
 def main():
     #get the address and port 
     address,port = trans_arg()
     #init the server
-    server = ThreadTcpServer(address,port,HttpHandler)
+    server = ThreadTcpServer(address,port,MyHttpHandler)
+
+    #server = ThreadTcpServer(address,port,MyTcpHandler)
     #run it
     try: 
         server.serve_forever()
